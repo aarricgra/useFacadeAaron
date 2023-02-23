5026f3fa33c2a344317c0d3453d0cf4f61bac46d
aarricgra <aarricgra@alu.edu.gva.es>
2023-02-23 13:43:14 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+import com.google.zxing.WriterException;
+
+public class Main {
+    public static void main(String[] args) throws WriterException {
+            new CodigoQR("https://jitpack.io/#aarricgra/QrLib/0.2");
+    }
+}
